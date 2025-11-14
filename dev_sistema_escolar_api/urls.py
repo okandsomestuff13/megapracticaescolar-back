ed03fa2593029dcb5b2416e8243ee6c155a9d260
okandsomestuff13 <dayradelrep@gmail.com>
2025-11-14 11:07:08 -0600

    Hasta listar maestros front

---
+++ dev_sistema_escolar_api/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views.bootstrap import VersionView
+from dev_sistema_escolar_api.views import bootstrap
+from dev_sistema_escolar_api.views import users
+from dev_sistema_escolar_api.views import alumnos
+from dev_sistema_escolar_api.views import maestros
+from dev_sistema_escolar_api.views import auth
+
+urlpatterns = [
+   #Create Admin
+        path('admin/', users.AdminView.as_view()),
+    #Admin Data
+        path('lista-admins/', users.AdminAll.as_view()),
+    #Edit Admin
+        #path('admins-edit/', users.AdminsViewEdit.as_view())
+    #Create Alumno
+        path('alumnos/', alumnos.AlumnosView.as_view()),
+    #Create Maestro
+        path('maestros/', maestros.MaestrosView.as_view()),
+    #Maestro Data
+        path('lista-maestros/', maestros.MaestrosAll.as_view()),
+    #Login
+        path('login/', auth.CustomAuthToken.as_view()),
+    #Logout
+        path('logout/', auth.Logout.as_view())
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
